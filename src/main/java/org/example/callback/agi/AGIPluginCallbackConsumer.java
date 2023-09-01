c03de81252fca15ebfd867693d57ea11739c3f83
zeymo <zeymo.wang@gmail.com>
2023-09-01 14:23:01 +0800

    feat(#agi): support agi plugin callback

--- src/main/java/org/example/callback/agi/AGIPluginCallbackConsumer.java
+++ src/main/java/org/example/callback/agi/AGIPluginCallbackConsumer.java
@@ -14,11 +14,15 @@ public class AGIPluginCallbackConsumer implements OpenDingTalkCallbackListener<D
     public DingTalkAGIPluginResponse execute(DingTalkAGIPluginRequest request) {
         log.info("receive AGI plugin request={}", request);
         DingTalkAGIPluginOutput output = new DingTalkAGIPluginOutput();
-        output.setOutput("echo");
+        switch (request.getAbilityKey()) {
+            case "hello" :
+                output.setOutput("echo");
+            default:
+                output.setOutput("no service");
+        }
         DingTalkAGIPluginResponse response = new DingTalkAGIPluginResponse();
         response.setRequestId(request.getRequestId());
         response.setResult(output);
         return response;
     }
-
 }
